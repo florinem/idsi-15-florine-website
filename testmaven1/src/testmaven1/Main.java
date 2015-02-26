fdcc6858705876bffef1d3afcb01f01d1e7aa79f
florinem <florine-monnier@hotmail.com>
2015-02-26 13:40:33 +0100

    first commit

---
+++ testmaven1/src/testmaven1/Main.java
@@ -0,0 +1,13 @@
+package testmaven1;
+
+import javax.mail.event.MailEvent;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello!!!!");
+		MailEvent mailEvent1 =null;
+	}
+
+}
